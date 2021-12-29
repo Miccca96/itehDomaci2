8a79134dc804c05d4afea6ce2036d91fa2b743e9
Miccca96 <milicavasovic96@gmail.com>
2021-12-29 11:35:30 +0100

    Kreirane putanje na sajtu i implementiran login i autentifikacija

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ItemController;
+use App\Http\Controllers\Api\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response("Failed login", 401);
+    }
+    return $user->createToken($user->email)->plainTextToken;
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('item', ItemController::class);
+    Route::apiResource('store', StoreController::class);
 });
