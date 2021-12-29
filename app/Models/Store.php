6cc7d1bfe2384f210807656ac7d609d6b89ba26e
Miccca96 <milicavasovic96@gmail.com>
2021-12-29 11:22:21 +0100

    Kreiran model item i migracija

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -15,4 +15,8 @@ class Store extends Model
     {
         return $this->belongsTo(StoreCategory::class);
     }
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
